ecdf46b02a4a772e223f9512b8cb0a7db2180a83
Kasper Lewau <lewauu@gmail.com>
2015-01-20 16:13:24 +0100

    Move require statements to separate file

--- app/app.rb
+++ app/app.rb
@@ -1,19 +1,6 @@
 require 'bundler/setup'
 Bundler.require
-
-require_relative 'modules/helpers'
-
-require_relative 'models/character'
-
-require_relative 'models/ability'
-require_relative 'models/abilities/execute'
-require_relative 'models/abilities/thunder_clap'
-require_relative 'models/abilities/whirlwind'
-require_relative 'models/abilities/mortal_strike'
-
-require_relative 'models/mod'
-require_relative 'models/mods/mastery_mod'
-require_relative 'models/mods/versatility_mod'
+require_relative 'requires'
 
 @char    = Character.new(Helpers::parse_json(ARGV[0]))
 @targets = (ARGV[1] || 1).to_f
---
+++ app/requires.rb
@@ -0,0 +1,13 @@
+require_relative 'modules/helpers'
+
+require_relative 'models/character'
+
+require_relative 'models/ability'
+require_relative 'models/abilities/execute'
+require_relative 'models/abilities/thunder_clap'
+require_relative 'models/abilities/whirlwind'
+require_relative 'models/abilities/mortal_strike'
+
+require_relative 'models/mod'
+require_relative 'models/mods/mastery_mod'
+require_relative 'models/mods/versatility_mod'
