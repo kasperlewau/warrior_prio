d8952978a089f192d060e9076bea188369b68d64
Kasper Lewau <lewauu@gmail.com>
2015-01-22 10:53:21 +0100

    Update Mastery Formula.
    Value per point decreased by roughly 18 percentage points.

--- app/models/mods/mastery_mod.rb
+++ app/models/mods/mastery_mod.rb
@@ -1,9 +1,8 @@
 class MasteryMod < Mod
   BASE      = 880
-  PER_POINT = 0.03181988743
-  HOTFIX    = (5.5/3.5)
+  PER_POINT = 0.04091074681
 
   def apply(value)
-    ((((value + BASE) * PER_POINT) / 100) * HOTFIX ) + 1.0
+    (((value + BASE) * PER_POINT) / 100) + 1.0
   end
 end
