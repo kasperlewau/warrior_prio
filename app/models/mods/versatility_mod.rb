9f640e29ffb14cfdf6d19640c2ca3ee8a24a9a72
Kasper Lewau <lewauu@gmail.com>
2015-01-22 10:56:54 +0100

    Add a base value to Versatility, update the per point formula.

--- app/models/mods/versatility_mod.rb
+++ app/models/mods/versatility_mod.rb
@@ -1,7 +1,8 @@
 class VersatilityMod < Mod
-  PER_POINT = 0.011888111888111888
+  BASE      = 100
+  PER_POINT = 0.01171581769
 
   def apply(value)
-    ((value * PER_POINT) / 100.0 ) + 1.0
+    (((value + BASE) * PER_POINT) / 100.0 ) + 1.0
   end
 end
