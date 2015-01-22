d895aeb631116520d69cfa570ca82086b31657d6
Kasper Lewau <lewauu@gmail.com>
2015-01-22 13:08:33 +0100

    Add note about mismatching tooltip multipliers

--- app/app.rb
+++ app/app.rb
@@ -9,26 +9,27 @@ require_relative 'requires'
 
 @reg_mods = {
   fury_execute_hotfix: Mod.new(multiplier: 1.10),
-  seasoned:    Mod.new(multiplier: 1.10),
-  mastery:     Mod.new(multiplier: @char.mastery),
-  versatility: Mod.new(multiplier: @char.versatility),
-  tc_glyph:    Mod.new(multiplier: 1.50),
-  ww_hotfix:   Mod.new(multiplier: 0.70),
-  cs:          CsMod.new(multiplier: 1.3497, targets: @targets)
+  seasoned:     Mod.new(multiplier: 1.10),
+  mastery:      Mod.new(multiplier: @char.mastery),
+  versatility:  Mod.new(multiplier: @char.versatility),
+  tc_glyph:     Mod.new(multiplier: 1.50),
+  ww_hotfix:    Mod.new(multiplier: 0.70),
+  target_armor: Mod.new(multiplier: 0.6503),
+  cs:           CsMod.new(multiplier: 1.3497, targets: @targets)
 }
 
 @abilities = {
   ww:       Whirlwind.new(name: 'Whirlwind', cost: 20.00, base: 2.00),
   ms:       MortalStrike.new(name: 'Mortal Strike', cost: 20.00, base: 2.251),
   ms_cs:    MortalStrike.new(name: 'Mortal Strike', cost: 20.00, base: 2.251, with: ['cs']),
-  tc:       ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40),
-  tc_cs:    ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['cs']),
-  tc_g:     ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['tc_glyph']),
-  tc_g_cs:  ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['cs', 'tc_glyph']),
-  ex_10:    Execute.new(name: 'Execute', cost: 10.00, base: 1.50),
-  ex_40:    Execute.new(name: 'Execute', cost: 40.00, base: 1.50),
-  ex_10_cs: Execute.new(name: 'Execute', cost: 10.00, base: 1.50, with: ['cs']),
-  ex_40_cs: Execute.new(name: 'Execute', cost: 40.00, base: 1.50, with: ['cs'])
+  tc:       ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40), # Tooltip assumes a 1.2 Base.
+  tc_cs:    ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['cs']), # Tooltip assumes a 1.2 Base.
+  tc_g:     ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['tc_glyph']), # Tooltip assumes a 1.2 Base.
+  tc_g_cs:  ThunderClap.new(name: 'Thunder Clap', cost: 10.00, base: 1.40, with: ['cs', 'tc_glyph']), # Tooltip assumes a 1.2 Base.
+  ex_10:    Execute.new(name: 'Execute', cost: 10.00, base: 1.50), # Tooltip assumes a 1.6 base.
+  ex_40:    Execute.new(name: 'Execute', cost: 40.00, base: 1.50), # Tooltip assumes a 1.6 base.
+  ex_10_cs: Execute.new(name: 'Execute', cost: 10.00, base: 1.50, with: ['cs']), # Tooltip assumes a 1.6 base.
+  ex_40_cs: Execute.new(name: 'Execute', cost: 40.00, base: 1.50, with: ['cs']) # Tooltip assumes a 1.6 base.
 }
 
 @table_data = @abilities.map { |k, ability| ability.table_data(@char, @reg_mods, @targets) }
